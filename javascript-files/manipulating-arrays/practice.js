b8a6baa3f06fc98c2e10a40538b139afc1c169a9
Roniel <ronielr192@gmail.com>
2023-01-19 10:13:31 -0300

    added practice file

---
+++ javascript-files/manipulating-arrays/practice.js
@@ -0,0 +1,14 @@
+// Make a stack and a queue
+
+let myStack = [];
+myStack.push(1, 2, 3, 4, 5);
+console.log(myStack);
+myStack.pop();
+console.log(myStack);
+
+let myQueue = [];
+myQueue.unshift('a', 'b', 'c', 'd', 'e');
+console.log(myQueue);
+myQueue.shift();
+myQueue.push(1);
+console.log(myQueue);
